7e28598092583a251d87f2ac9dd95901067e9d19
Mika Rautio <mika.rautio@gmail.com>
2024-06-04 11:50:20 +0300

    Initial commit

---
+++ Demo.js
@@ -0,0 +1,40 @@
+/*
+intro
+    Kielletty/seis käsimerkki, liikenteen hälyä, epämääräisesti näkyviä autoja taustalla
+    Käsi menee nyrkkiin
+
+transitio
+    tärinää, tausta feidaa
+
+nyrkki
+    nyrkki lentää avaruudessa ja tuhoaa planeettoja järjestyksessä (myös pluton lol)
+    laskeutuu maapallolle meteoriittityyliin (ks se joku conspiracyn intro)
+
+transitio
+    nyrkistä kasvaa kukka
+
+ak-47
+    kukasta kasvaa monta ak-47:aa
+
+transitio jotenkin ak-47 -> ihmisen käsi (ehkä sama nyrkki hihan kanssa?)
+
+silhuettiskene
+    ak-47 jonkun kädessä ampuu kohti tuntematonta
+    tuntematon on jättiläiskissa
+    jättiläiskissa tuhoaa kaupunkia
+    kissaan osuu ydinohjus
+
+pääkallokissa
+    kiljuva kissapääkallo
+    kaleidoskooppisekoilu
+end
+    sydän missä lukee jumalauta ja sydämessä mp5 kuten amorin nuoli
+*/
+
+Demo.prototype.init = function () {
+  const settings = new Settings();
+  //settings.demo.sync.rocketFile = 'sync/auto.rocket';
+  settings.demo.sync.beatsPerMinute = 120;
+  settings.demo.sync.rowsPerBeat = 8;
+
+};
