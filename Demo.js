36f37e8e79a5d336368410c291def1fde6be0b80
Mika Rautio <mika.rautio@gmail.com>
2024-06-08 15:11:09 +0300

    Preliminary space effect https://github.com/jumalauta/demo-jml-asm24/issues/11

--- Demo.js
+++ Demo.js
@@ -35,6 +35,59 @@ Demo.prototype.sceneIntro = function () {
   this.loader.setScene('intro');
 };
 
+Demo.prototype.sceneSpace = function () {
+  this.loader.setScene('space');
+
+  let stars = new Array(10000);
+  const min = -100;
+  const max  = 200;
+  for (let i = 0; i < stars.length; i++) {
+    stars[i] = {
+      "x1": Math.random() * max + min,
+      "y1": Math.random() * max + min,
+      "z1": i/stars.length * min,
+    };
+    stars[i].z2 = stars[i].z1 - min;
+    stars[i].startTime = 0;
+  }
+
+  this.loader.addAnimation({
+    "image": "star.png",
+    "perspective": "3d",
+    "billboard": true,
+    "instancer": {
+      "count": stars.length,
+      "runInstanceFunction": (properties) => {
+        const i = properties.index;
+        const count = properties.count;
+        const time = properties.time;
+        let object = properties.object;
+        let color = properties.color;
+
+        const scale = 0.5;
+        object.scale.x = scale;
+        object.scale.y = scale;
+    
+        object.position.x = stars[i].x1;
+        object.position.y = stars[i].y1;
+        const percent = (time-stars[i].startTime)/5.;
+        object.position.z = Utils.mix(stars[i].z1, stars[i].z2, percent);
+        if (object.position.z > 0 || time < stars[i].startTime) {
+          stars[i] = {
+            "x1": Math.random() * max + min,
+            "y1": Math.random() * max + min,
+            "z1": i/stars.length * min,
+          };
+          stars[i].z2 = stars[i].z1 - min;
+          stars[i].startTime = time; 
+        }
+    
+        color.a = 0.6 + 0.4*percent;    
+      }
+    }
+  });
+}
+
 Demo.prototype.sceneSkullCat = function () {
   this.loader.setScene('skullCat');
   this.loader.addAnimation({
@@ -58,9 +111,11 @@ Demo.prototype.init = function () {
   settings.demo.sync.rowsPerBeat = 8;
 
   this.sceneIntro();
+  this.sceneSpace();
   this.sceneSkullCat();
 
   this.loader.setScene('main');
   this.loader.addAnimation({"start": start, "duration": 10, "scene":{"name":"intro"}});
-  this.loader.addAnimation({"start": start, "duration": duration, "scene":{"name":"skullCat"/*, "fbo":{"name":"skullCatFbo"}*/}});
+  this.loader.addAnimation({"start": start, "duration": 30, "scene":{"name":"space"/*, "fbo":{"name":"skullSpaceFbo"}*/}});
+  this.loader.addAnimation({"start": start+30, "duration": 30, "scene":{"name":"skullCat"/*, "fbo":{"name":"skullCatFbo"}*/}});
 };
