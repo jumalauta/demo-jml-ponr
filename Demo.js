96e58dff9683029537d1d7461c510cedc9318d7d
saul-l <sauli.lehtinen@gmail.com>
2024-07-14 16:41:32 +0300

    Cat skull scene started + temp syncs

--- Demo.js
+++ Demo.js
@@ -155,8 +155,8 @@ Demo.prototype.init = function () {
     {start: start+8*pattern, duration: 8*pattern, name: 'space'},
     {start: start+16*pattern, duration: 3*pattern, name: 'earthHit'},
     {start: start+19*pattern , duration: 8*pattern, name: 'treeGrow'},
-    {start: start+27*pattern , duration: 8*pattern, name: 'catBattle'},
-    {start: start+444, duration: 30, name: 'skullCat'},
+    {start: start+27*pattern , duration: 7*pattern, name: 'catBattle'},
+    {start: start+34*pattern, duration: 16*pattern, name: 'skullCat'},
   ];
 
   scenes.forEach((scene) => {
--- sceneSkull/Skull.js
+++ sceneSkull/Skull.js
@@ -10,4 +10,40 @@ Demo.prototype.sceneSkullCat = function () {
       ]
     }
   });
+
+  this.loader.addAnimation({
+    "light": {
+        "type": "Directional",
+        "properties": { "intensity": 0.55 },
+        "castShadow": false
+    }
+    ,"color": [{
+      "r": 1.0, "g": 1.0, "b": 1.0
+    }]
+    ,"position": [{
+      "x": ()=>window.camPos[0], "y": ()=>window.camPos[1], "z": ()=>window.camPos[2]
+    }]
+  });
+
+  this.loader.addAnimation([{
+    "object":{
+      "name":"sceneSkull/catskull.gltf",
+      "time":10.0,
+      "animations": {
+        "scream":  {"weight":()=>Sync.get('CatSkull:Scream'), "timescale":1.0, "enabled":true, "loop":false}
+      }
+    }
+   ,"position":[{
+      "x":0,
+      "y":0,
+      "z":0
+    }]
+   ,"angle":[{
+      "degreesY":-90,
+      "degreesX":0,
+      "degreesZ":0
+	  }]
+   ,"scale":[{"uniform3d":1.03}]
+  }]);
+
 };
