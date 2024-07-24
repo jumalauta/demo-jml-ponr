c07fe94289815bb6c6ad6bd0aa09541020e256f4
Mika Rautio <1129758+mrautio@users.noreply.github.com>
2024-07-24 10:18:47 +0300

    Rocket bindings for skull background

--- sceneSkull/Skull.js
+++ sceneSkull/Skull.js
@@ -13,33 +13,32 @@ Demo.prototype.sceneSkullCat = function () {
     "image": ["skullCarBackgroundFbo.color.fbo"],
     "shader":{"name":"sceneSkull/background.fs",
       "variable": [
+        // chainEffectN value = <baseeffect>.<mix amount = .0 (all), .999 (minimum)>
+        {"name":"chainEffect0", "value":[()=>Sync.get('CatSkullBg:e0')]},
+        {"name":"chainEffect1", "value":[()=>Sync.get('CatSkullBg:e1')]},
+        {"name":"chainEffect2", "value":[()=>Sync.get('CatSkullBg:e2')]},
+        {"name":"chainEffect3", "value":[()=>Sync.get('CatSkullBg:e3')]},
+        {"name":"chainEffect4", "value":[()=>Sync.get('CatSkullBg:e4')]},
+        {"name":"chainEffect5", "value":[()=>Sync.get('CatSkullBg:e5')]},
+        {"name":"chainEffect6", "value":[()=>Sync.get('CatSkullBg:e6')]},
+        {"name":"chainEffect7", "value":[()=>Sync.get('CatSkullBg:e7')]},
+        {"name":"chainEffect8", "value":[()=>Sync.get('CatSkullBg:e8')]},
+        {"name":"chainEffect9", "value":[()=>Sync.get('CatSkullBg:e9')]},
         // chaineffect base effect numbers:
         // 0: no-operation
         // 1: texcoordinate bias
-        {"name":"coordBias","value":[()=>getSceneTimeFromStart()*0.1,()=>0.0]},
+        {"name":"coordBias","value":[()=>Sync.get('CatSkullBg:coordBiasX'),()=>Sync.get('CatSkullBg:coordBiasY')]},
         // 2: texcoordinate bias 2
-        {"name":"coordBias2", "value":[()=>getSceneTimeFromStart(), ()=>getSceneTimeFromStart()]},
+        {"name":"coordBias2", "value":[()=>Sync.get('CatSkullBg:coordBias2X'),()=>Sync.get('CatSkullBg:coordBias2Y')]},
         // 3: kaleidoscope
-        {"name":"kaleidoscopeXangle","value":[()=>((Math.sin(getSceneTimeFromStart())+1)/2.0)*10.0+1.0]},
+        {"name":"kaleidoscopeXangle","value":[()=>Sync.get('CatSkullBg:kaleidoAngle')]},
         // 4: funky deformation
         // 5: rotozoom
-        {"name":"angle", "value":[()=>3.14/4.*Math.sin(getSceneTimeFromStart())]},
-        {"name":"zoom", "value":[()=>2.56*Math.sin(getSceneTimeFromStart())]},
+        {"name":"angle", "value":[()=>Sync.get('CatSkullBg:rotoAngle')]},
+        {"name":"zoom", "value":[()=>Sync.get('CatSkullBg:rotoZoom')]},
         // 6: tunnel
         // 7: plasma deformation
-        {"name":"scale", "value":[()=>5., ()=>5.]},
-
-        // chainEffectN value = <baseeffect>.<mix amount = .0 (all), .999 (minimum)>
-        {"name":"chainEffect0", "value":[()=>3.7]},
-        {"name":"chainEffect1", "value":[()=>7.6]},
-        {"name":"chainEffect2", "value":[()=>1.3]},
-        {"name":"chainEffect3", "value":[()=>4.7]},
-        {"name":"chainEffect4", "value":[()=>7.8]},
-        {"name":"chainEffect5", "value":[()=>3.0]},
-        {"name":"chainEffect6", "value":[()=>3.0]},
-        {"name":"chainEffect7", "value":[()=>0]},
-        {"name":"chainEffect8", "value":[()=>0]},
-        {"name":"chainEffect9", "value":[()=>0]},
+        {"name":"scale", "value":[()=>Sync.get('CatSkullBg:plasmaScaleX'), ()=>Sync.get('CatSkullBg:plasmaScaleY')]},
       ]
     }
   });
